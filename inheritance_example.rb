36139a2955120a74db32634047c7bb63dcabd60f
gbactualize <gbactualize@gmail.com>
2016-08-23 18:46:19 -0500

    Bike/Car inital commit

---
+++ inheritance_example.rb
@@ -0,0 +1,48 @@
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+  
+  def show_speed
+    puts @speed
+  end
+
+  def show_direction
+    puts @direction
+  end
+end
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+bike = Bike.new
+bike.ring_bell
+bike.accelerate
+bike.show_speed
+
+car = Car.new
+car.honk_horn
+car.turn("south")
+car.show_direction
